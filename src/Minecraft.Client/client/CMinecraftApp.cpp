31bead877dc01d4d74cbf483b138d804b6ceaffc
aleblbl <alell8632@gmail.com>
2026-02-01 06:53:02 +0100

    initial CMinecraftApp

---
+++ src/Minecraft.Client/client/CMinecraftApp.cpp
@@ -0,0 +1,71 @@
+#include "client/CMinecraftApp.h"
+
+
+
+//
+// TU2 .text 0x824eb558
+// this is not exactly matching, it is optimized away in TU2
+// so i'll reimplement based on the usage
+// it is used sometime to print error messages
+//
+void CMinecraftApp::DebugPrintf(char* fmt, ...) 
+{
+	return;
+	//va_list args;
+	//va_start(args, fmt);
+	//vprintf(fmt, args);
+	//va_end(args);
+}
+
+//
+// TU2 .text 8228caf8
+//
+//HRESULT CMinecraftApp::InitialiseXui()
+//{
+//	HRESULT hr;
+//	XUIInitParams xip;
+//
+//	
+//	if (this->isInit)
+//	{
+//		if (this->m_XDC == NULL) {
+//			hr = XuiRenderCreateDC(&this->m_XDC);
+//			if (FAILED(hr)) {
+//				return hr;
+//			}
+//		}
+//
+//		XUI_INIT_PARAMS(xip);
+//		hr = XuiInit(&xip);
+//		if (FAILED(hr)) {
+//			hr = XuiRenderDestroyDC(this->m_XDC);
+//			this->m_XDC = NULL;
+//			return hr;
+//		}
+//	}
+//
+//	this->unk3 = true;
+//}
+
+
+// TU2 .text 0x8228a7c0
+CMinecraftApp::CMinecraftApp() : pUnk1(nullptr), m_XDC(NULL), isInit(true) {
+	
+	QueryPerformanceFrequency(&this->PerfFreq);
+	QueryPerformanceCounter(&this->PerfCount);
+}
+
+// TU2 .text 8227ea28
+CXboxMinecraftApp::CXboxMinecraftApp() {
+	// TODO:
+
+	
+}
+
+
+// TU2 .text 8227ed70
+void CXboxMinecraftApp::RegisterXuiClasses()
+{
+	// TODO:
+	// STUBBBBBBB
+}
